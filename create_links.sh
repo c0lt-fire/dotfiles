51eee698077ae1b2ed9fc3ebb7b660a4951d3582
igor.b <igor.brevdo@gmail.com>
2019-10-26 13:53:33 +0300

    add gitconfig

--- create_links.sh
+++ create_links.sh
@@ -3,7 +3,7 @@
 # you should re-run this script only after a new file has been added under current directory
 
 dir="dotfiles"
-files="bashrc vimrc bash_profile face.icon conkyrc"
+files="bashrc vimrc bash_profile face.icon conkyrc gitconfig"
 
 
 #cd $dir
@@ -13,7 +13,7 @@ for file in $files; do
     if ! [ -L ~/.$file ] ; then
         rm ~/.$file
         echo "Creating link ~/.$file -> $dir/$file"
-        ln -s $dir/$file ~/.$file
+        ln -sv $dir/$file ~/.$file
     fi
 done
 
